478e75eda83dc1e4502ba18c99b5957a320132b9
Aleksandergreg <alek3691@stud.kea.dk>
2024-09-20 12:43:07 +0200

    initial commit

---
+++ src/main/java/com/example/teknologivirtuelmaskine/TeknologiVirtuelMaskineApplication.java
@@ -0,0 +1,13 @@
+package com.example.teknologivirtuelmaskine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologiVirtuelMaskineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeknologiVirtuelMaskineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teknologivirtuelmaskine/TeknologiVirtuelMaskineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teknologivirtuelmaskine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiVirtuelMaskineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
